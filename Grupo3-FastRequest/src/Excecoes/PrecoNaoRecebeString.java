88f18a1d94183a8093c2d36476fe31ad01e7a062
Antonio Carvalho de Oliveira Junior <_antonio_@hotmail.com>
2013-06-05 16:59:24 -0300

    Tratando quando se coloca string no campo preco

---
+++ Grupo3-FastRequest/src/Excecoes/PrecoNaoRecebeString.java
@@ -0,0 +1,30 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Excecoes;
+
+/**
+ *
+ * @author Antonio
+ */
+public class PrecoNaoRecebeString extends RuntimeException{
+    
+    public PrecoNaoRecebeString(){
+    super();
+    }
+
+    public PrecoNaoRecebeString(Throwable cause) {
+        super(cause);
+    }
+
+    public PrecoNaoRecebeString(String message) {
+        super(message);
+    }
+
+    public PrecoNaoRecebeString(String message, Throwable cause) {
+        super(message, cause);
+    }
+   
+    
+}
