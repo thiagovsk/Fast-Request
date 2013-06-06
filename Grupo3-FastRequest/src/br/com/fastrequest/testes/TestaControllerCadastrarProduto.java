9e04b2d4c22f9979a8c29c15dc04503f16388419
Yeltsin <yeltsin.lobato@hotmail.com>
2013-06-06 03:08:50 -0300

    Implementando Teste

---
+++ Grupo3-FastRequest/src/br/com/fastrequest/testes/TestaControllerCadastrarProduto.java
@@ -0,0 +1,28 @@
+package br.com.fastrequest.testes;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import br.com.fastrequest.controller.Controller;
+import br.com.fastrequest.dao.ConexaoBD;
+import br.com.fastrequest.dao.ProdutoDAO;
+import br.com.fastrequest.model.Produto;
+
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestaControllerCadastrarProduto {
+	
+	
+	@Test
+	public void testaSeObjetosForamInstanciados() {
+		
+		Produto produto = new Produto();	
+		ProdutoDAO dao = new ProdutoDAO();
+		
+		assertNotNull(produto);
+		assertNotNull(dao);
+	}
+}
