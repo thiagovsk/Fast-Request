9df79c1f36de7012d2c738ab7665c03c0b0a2edb
Yeltsin <yeltsin.lobato@hotmail.com>
2013-06-24 13:24:51 -0300

    Commit tela Cliente

--- Grupo3-FastRequest/src/br/com/fastrequest/view/TelaCliente.java
+++ Grupo3-FastRequest/src/br/com/fastrequest/view/TelaCliente.java
@@ -345,4 +345,5 @@ public class TelaCliente extends javax.swing.JFrame {
     private javax.swing.JTextField jTextField5;
     private javax.swing.JTextField jTextField6;
     // End of variables declaration//GEN-END:variables
+    //...
 }
