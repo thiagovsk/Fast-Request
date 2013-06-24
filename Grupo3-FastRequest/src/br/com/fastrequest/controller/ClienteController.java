1a652db602e7a079a9cca1897c5a2dbe4c72cc3a
Priscilla Gonçalves da Silva e Souza <priscilla_hasya@windowslive.com>
2013-06-24 11:23:51 -0300

    refatorandométodo salvar e identação na classe ClienteController

--- Grupo3-FastRequest/src/br/com/fastrequest/controller/ClienteController.java
+++ Grupo3-FastRequest/src/br/com/fastrequest/controller/ClienteController.java
@@ -21,10 +21,8 @@ import br.com.fastrequest.model.Cliente;
 public class ClienteController {
     
     
-	  public void salvar(int id, String nome, String pratoPreferido, String email,
-			String telefone) throws SQLException, ParseException {
+	  public void salvar( String nome, String pratoPreferido, String email, String telefone) throws SQLException, ParseException {
 	        Cliente cliente = new Cliente();
-	        cliente.setId(id);
 	        cliente.setNome(nome);
 	        cliente.setPratoPreferido(pratoPreferido);
                 cliente.setEmail(email);
@@ -32,8 +30,9 @@ public class ClienteController {
                 
 	        new ClienteDAO().salvar(cliente);
 	    }
-	  public void alterar(int id, String nome, String pratoPreferido, String email,
-			String telefone) throws ParseException, SQLException {
+          
+          
+	  public void alterar(int id, String nome, String pratoPreferido, String email, String telefone) throws ParseException, SQLException {
                 Cliente cliente = new Cliente();
 	        cliente.setId(id);
 	        cliente.setNome(nome);
@@ -43,22 +42,29 @@ public class ClienteController {
                 
 	        new ClienteDAO()).alterar(cliente);
 	    }
+          
+          
 	  public List<Cliente> listaClientes() {
 	        ClienteDAO clienteDao = new ClienteDAO();
-	        try {
+	       
+                try {
 	            return clienteDAO.encontrarCliente();
-	        } catch (SQLException e) {
+	      
+                } catch (SQLException e) {
 	            JOptionPane.showMessageDialog(null, "Problemas ao localizar contato\n" + e.getLocalizedMessage());
 	        }
 	        return null;
-	    }
+	  }
+          
+          
 	  public void excluir(String nome) throws SQLException {
 	        new ClienteDAO().excluir(nome);
-	    }
+          }
 	 
-	    public Cliente buscaContatoPorNome(String nome) throws SQLException {
+          
+	  public Cliente buscaContatoPorNome(String nome) throws SQLException {
 	        ClienteDAO clienteDAo = new ClienteDAO();
 	        return clienteDAo.encontrarNome(nome);
-	    }
+	  }
     
 }
