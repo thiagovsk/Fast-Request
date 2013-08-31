09869786b828af15322275b13dc2524b9c68f3a1
Fillipe Oliveira Feitosa <fillfeitosa@gmail.com>
2013-08-31 15:00:18 -0300

    [CoreClasses] Estrutura básica das classes base

---
+++ DataBaseAnalize/src/databaseanalize/coreclasses/CheckEmail.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package databaseanalize.coreclasses;
+/**
+ *
+ * @author Fillipe Feitosa
+ * fillipe_feitosa@yahoo.com.br
+ *
+ */
+public class CheckEmail implements CheckConsistency{
+
+}
---
+++ DataBaseAnalize/src/databaseanalize/coreclasses/CheckType.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package databaseanalize.coreclasses;
+
+/**
+ *
+ * @author Fillipe Feitosa
+ * fillipe_feitosa@yahoo.com.br
+ *
+ */
+public class CheckType implements CheckConsistency{
+
+}
