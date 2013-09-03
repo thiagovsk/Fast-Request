b7faef45f09e96674d2d28afdce064fd15c96e70
Cris <cris>
2013-09-02 21:34:47 -0300

    Class implementation CheckEmail

--- DataBaseAnalize/src/databaseanalize/coreclasses/CheckEmail.java
+++ DataBaseAnalize/src/databaseanalize/coreclasses/CheckEmail.java
@@ -2,6 +2,7 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
+import java.util.regex.*;
 
 package databaseanalize.coreclasses;
 /**
@@ -10,6 +11,32 @@ package databaseanalize.coreclasses;
  * fillipe_feitosa@yahoo.com.br
  *
  */
+
 public class CheckEmail{
+
+	public CheckEmail(String email){
+		this.setEmail(email);
+	}
+	
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	public static boolean valide(string email){
+
+	boolean isEmailValid=false;
+	if(email != null && email.length()>0){
+	    String expression = "^[\\w-]+(\\.[\\w-]+)*@([\\w-]+\\.)+[a-zA-Z]{2,4}$";
+	    Pattern pattern = Pattern.compile(expression,Pattern.CASE_INSENSITIVE);
+	    Matcher matcher = pattern.matcher(email);
+	    if(matcher.matches()){
+	    	isEmailValid=true;
+	    }
+	}
+	return isEmailValid;
+	}
 
 }
